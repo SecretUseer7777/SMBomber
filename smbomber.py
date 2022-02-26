9b9c58ef9f17b17557daca480fc19dd891c33311
SecretUseer7777 <99134759+SecretUseer7777@users.noreply.github.com>
2022-02-26 22:10:41 +0200

    .

--- smbomber.py
+++ smbomber.py
@@ -7,35 +7,27 @@ import argparse
 # Go to current dir
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-try:
-    from tools.crash import CriticalError
-    import tools.addons.clean
-    import tools.addons.logo
-    import tools.addons.winpcap
-    from tools.method import AttackMethod
-except ImportError as err:
-    CriticalError("Failed import some modules", err)
-    sys.exit(1)
+
 
 # Parse args
 parser = argparse.ArgumentParser(description="Denial-of-service ToolKit")
 parser.add_argument(
-    "--target",
+    "-ip",
     type=str,
     metavar="<IP:PORT, URL, PHONE>",
     help="Target ip:port, url or phone",
 )
 parser.add_argument(
-    "--method",
+    "-m",
     type=str,
     metavar="<SMS/EMAIL/NTP/UDP/SYN/ICMP/POD/SLOWLORIS/MEMCACHED/HTTP>",
     help="Attack method",
 )
 parser.add_argument(
-    "--time", type=int, default=10, metavar="<time>", help="time in secounds"
+    "-ti", type=int, default=10, metavar="<time>", help="time in secounds"
 )
 parser.add_argument(
-    "--threads", type=int, default=3, metavar="<threads>", help="threads count (1-200)"
+    "-t", type=int, default=3, metavar="<threads>", help="threads count (1-200)"
 )
 
 # Get args
